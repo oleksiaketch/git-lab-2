f53fd3de0e02a92c7a40abdeb473ca3a712808fe
Luke Oleksiak <lo853110@ohio.edu>
2025-09-03 17:11:36 -0400

    added code

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Luke Oleksiak>
+ *        Date: <Septemeber 3, 2025>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
